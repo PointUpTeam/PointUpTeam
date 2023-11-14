28323e6cf70fa11249fcdbecca39192ad5e8f5ed
EncoderIV <145885774+EncoderIV@users.noreply.github.com>
2023-11-14 08:37:19 -0500

    fixed buggy popup

--- docs/download.js
+++ docs/download.js
@@ -1,18 +1,20 @@
-const popup = document.getElementById("popup") ;
-const popupBtn = document.getElementById("downloadBtn") ;
-const popupCloseBtn = document.getElementById("popup-close") ;
-
-popupBtn.onclick = function () {
-    popup.style.display = "block" ;
-}
-
-popupCloseBtn.onclick = function() {
-    popup.style.display = "none" ; 
-}
-
-
-window.onclick = function(event) {
-    if( event.target === popup ){
-        popup.style.display = "none"
+document.addEventListener('DOMContentLoaded', (event) => {
+    const popup = document.getElementById("Popup") ;
+    const popupBtn = document.getElementById("downloadBtn") ;
+    const popupCloseBtn = document.getElementById("CloseBtnPopup") ;
+    
+    popupBtn.onclick = function () {
+        popup.style.display = "block" ;
     }
-}
+    
+    popupCloseBtn.onclick = function() {
+        popup.style.display = "none" ; 
+    }
+    
+    
+    window.onclick = function(event) {
+        if( event.target === popup ){
+            popup.style.display = "none"
+        }
+    }
+});
